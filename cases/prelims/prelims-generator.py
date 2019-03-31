a0b20caeb751e38297b3bb33155a38b722fe8956
Francis dela Cruz <franciszac.dlc@gmail.com>
2019-03-31 13:57:30 +0800

    Finish prelims generator

--- cases/prelims/prelims-generator.py
+++ cases/prelims/prelims-generator.py
@@ -38,37 +38,9 @@ def _remove_tmp_files():
         except OSError as e:
             print 'Removing tmp files error:', e
 
-### Extract the base map
-## Filename: 4leg-<trafficdemand>-<cycletime>.lmf
-zip_file = zipfile.ZipFile('4leg-new-450-150.lmf')
-zip_file.extractall()
-
 ### Process the files
 
 center_lanes = [246,247,248,249]
-
-# for demand in traffic_demand:
-#     with open(_tmp_files['dispatcher'], 'r+') as f:
-#         data = json.load(f)
-#         for dispatcher in data['head']:
-#             if (dispatcher['ars'][2] == 0):
-#                 continue
-
-#             lane_scaleby = 1
-#             if (dispatcher['id'] in center_lanes):
-#                 lane_scaleby = 2
-
-#             scaleby = float(demand) / 450.0
-#             dispatcher['ars'][1]['0,3600'][0] = demand * lane_scaleby
-#             dispatcher['ars'][1]['0,3600'][1] = 0.125 * scaleby
-#         f.seek(0)
-#         json.dump(data, f, indent=4)
-#         f.truncate()
-
-#     ### Write to a new output file
-#     output_file = zipfile.ZipFile('output/4leg-new-{}-150.lmf'.format(demand), 'w')
-#     for f in _tmp_files.iteritems():
-#         output_file.write(f[1])
 
 default_cycle = 150
 default_greentimes = [25, 40, 25, 40]
@@ -77,28 +49,49 @@ default_lefttime = 30 # 0.4 of half of cycle
 default_throughtime = 45 # 0.6 of half of cycle
 
 for cycle in stoplight_cycle:
-    zip_file = zipfile.ZipFile('4leg-new-450-150.lmf')
-    zip_file.extractall()
-    with open(_tmp_files['control'], 'r+') as f:
-        data = json.load(f)
-        for control in data['head']:
-            lefttime = (int) (0.4 * cycle / 2)
-            throughtime = (cycle / 2) - lefttime
+    for demand in traffic_demand:
+        ### Extract the base map
+        ## Filename: 4leg-new-<trafficdemand>-<cycletime>.lmf
+        zip_file = zipfile.ZipFile('4leg-new-450-150.lmf')
+        zip_file.extractall()
 
-            offsets = [0, lefttime, lefttime+throughtime, (2*lefttime)+throughtime]
-            greentimes = [lefttime - 5, throughtime - 5]
-            greentimes = greentimes + greentimes
+        with open(_tmp_files['control'], 'r+') as f:
+            data = json.load(f)
+            for control in data['head']:
+                lefttime = (int) (0.4 * cycle / 2)
+                throughtime = (cycle / 2) - lefttime
 
-            phase = default_offsets.index(control['kws']['start'])
-            control['kws']['start'] = offsets[phase]
-            control['ars'][3][0] = cycle - greentimes[phase] - 3
-            control['ars'][3][1] = greentimes[phase]
-            control['ars'][3][2] = 3
-        f.seek(0)
-        json.dump(data, f, indent=4)
-        f.truncate()
+                offsets = [0, lefttime, lefttime+throughtime, (2*lefttime)+throughtime]
+                greentimes = [lefttime - 5, throughtime - 5]
+                greentimes = greentimes + greentimes
 
-    ### Write to a new output file
-    output_file = zipfile.ZipFile('output/4leg-new-450-{}.lmf'.format(cycle), 'w')
-    for f in _tmp_files.iteritems():
-        output_file.write(f[1])
+                phase = default_offsets.index(control['kws']['start'])
+                control['kws']['start'] = offsets[phase]
+                control['ars'][3][0] = cycle - greentimes[phase] - 3
+                control['ars'][3][1] = greentimes[phase]
+                control['ars'][3][2] = 3
+            f.seek(0)
+            json.dump(data, f, indent=4)
+            f.truncate()
+
+        with open(_tmp_files['dispatcher'], 'r+') as f:
+            data = json.load(f)
+            for dispatcher in data['head']:
+                if (dispatcher['ars'][2] == 0):
+                    continue
+
+                lane_scaleby = 1
+                if (dispatcher['id'] in center_lanes):
+                    lane_scaleby = 2
+
+                scaleby = float(demand) / 450.0
+                dispatcher['ars'][1]['0,3600'][0] = demand * lane_scaleby
+                dispatcher['ars'][1]['0,3600'][1] = 0.125 * scaleby
+            f.seek(0)
+            json.dump(data, f, indent=4)
+            f.truncate()
+
+        ### Write to a new output file
+        output_file = zipfile.ZipFile('output/4leg-new-{}-{}.lmf'.format(demand, cycle), 'w')
+        for f in _tmp_files.iteritems():
+            output_file.write(f[1])
