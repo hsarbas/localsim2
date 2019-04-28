70255066ba08a8015d4dfcb822a7f8850fbe0a23
Francis dela Cruz <franciszac.dlc@gmail.com>
2019-04-28 16:22:38 +0800

    Read greentimes from dataframes (TODO: Double check correctness, yellow light processing)

--- 1.x/localsim/models/tso/const.py
+++ 1.x/localsim/models/tso/const.py
@@ -35,3 +35,56 @@ CELL_ZONE_MAPPING = {
 }
 
 SURVEY_ZONE_MAPPING = {'30': (2, 2, 0), '22': (3, 1, 2), '2c': (2, 0, 0), '24': (2, 2, 1), '25': (3, 2, 1), '26': (3, 1, 1), '27': (3, 0, 1), '20': (2, 2, 2), '21': (3, 2, 2), '1e': (3, 1, 2), '23': (3, 0, 2), '28': (2, 0, 1), '29': (3, 2, 1), '1a': (3, 1, 3), '0': (1, 0, 3), '3': (3, 1, 0), '2': (3, 2, 0), '5': (1, 0, 0), '1d': (3, 2, 2), '7': (2, 1, 1), '1f': (3, 0, 2), '9': (3, 1, 1), '8': (3, 2, 1), '3a': (3, 1, 3), '1c': (2, 0, 2), '4': (3, 0, 0), 'a': (3, 0, 1), '6': (1, 0, 1), '39': (3, 2, 3), '12': (1, 0, 2), '3b': (3, 0, 3), '1b': (3, 0, 3), 'b': (1, 0, 2), '13': (1, 0, 2), 'd': (3, 2, 2), '11': (1, 0, 3), '10': (1, 0, 3), 'c': (2, 1, 2), '38': (2, 0, 3), '15': (1, 0, 1), '14': (1, 0, 1), '17': (1, 0, 0), 'f': (3, 0, 2), '19': (3, 2, 3), '32': (3, 1, 0), '31': (3, 2, 0), '16': (1, 0, 0), '37': (3, 0, 3), '36': (3, 1, 3), '35': (3, 2, 3), '34': (2, 1, 3), '2d': (3, 2, 0), '2e': (3, 1, 0), '2f': (3, 0, 0), '1': (2, 1, 0), '2a': (3, 1, 1), '2b': (3, 0, 1), '18': (2, 2, 3), '33': (3, 0, 0), 'e': (3, 1, 2)}
+
+STOPLIGHT_MAPPING = {
+    'approach0_lane1': (2,0,0),
+    'approach0_lane2': (2,1,0),
+    'approach0_lane3': (2,2,0),
+    'approach1_lane1': (2,0,1),
+    'approach1_lane2': (2,1,1),
+    'approach1_lane3': (2,2,1),
+    'approach2_lane1': (2,0,2),
+    'approach2_lane2': (2,1,2),
+    'approach2_lane3': (2,2,2),
+    'approach3_lane1': (2,0,3),
+    'approach3_lane2': (2,1,3),
+    'approach3_lane3': (2,2,3),
+}
+
+LEFT_TURN           = 0
+THROUGH_TURN        = 1
+RIGHT_TURN          = 2
+
+SOUTHBOUND          = 0
+WESTBOUND           = 1
+NORTHBOUND          = 2
+EASTBOUND           = 3
+
+PHASE_MAPPING = {
+    (0,0,0): [(2, LEFT_TURN, WESTBOUND), (2, RIGHT_TURN, NORTHBOUND)], # WBL, NBR
+    (0,0,1): [(2, THROUGH_TURN, EASTBOUND), (2, RIGHT_TURN, EASTBOUND)],
+    (0,1,0): [(2, LEFT_TURN, SOUTHBOUND), (2, RIGHT_TURN, WESTBOUND)],
+    (0,1,1): [(2, THROUGH_TURN, SOUTHBOUND), (2, RIGHT_TURN, SOUTHBOUND)],
+    (1,0,0): [(2, LEFT_TURN, EASTBOUND), (2, RIGHT_TURN, SOUTHBOUND)],
+    (1,0,1): [(2, THROUGH_TURN, WESTBOUND), (2, RIGHT_TURN, WESTBOUND)],
+    (1,1,0): [(2, LEFT_TURN, NORTHBOUND), (2, RIGHT_TURN, EASTBOUND)],
+    (1,1,1): [(2, THROUGH_TURN, NORTHBOUND), (2, RIGHT_TURN, NORTHBOUND)],
+}
+
+REVERSE_PHASE_MAPPING = {
+    (2, LEFT_TURN, SOUTHBOUND): [(0,1,0)],
+    (2, THROUGH_TURN, SOUTHBOUND): [(0,1,1)],
+    (2, RIGHT_TURN, SOUTHBOUND): [(0,1,1), (1,0,0)],
+    (2, LEFT_TURN, WESTBOUND): [(0,0,0)],
+    (2, THROUGH_TURN, WESTBOUND): [(1,0,1)],
+    (2, RIGHT_TURN, WESTBOUND): [(0,1,0), (1,0,1)],
+    (2, LEFT_TURN, NORTHBOUND): [(1,1,0)],
+    (2, THROUGH_TURN, NORTHBOUND): [(1,1,1)],
+    (2, RIGHT_TURN, NORTHBOUND): [(0,0,0), (1,1,1)],
+    (2, LEFT_TURN, EASTBOUND): [(1,0,0)],
+    (2, THROUGH_TURN, EASTBOUND): [(0,0,1)],
+    (2, RIGHT_TURN, EASTBOUND): [(1,1,0), (0,0,1)],
+}
+
+TIME_RANGE = 30
+TIME_STEP = 2
--- 1.x/localsim/models/tso/controller.py
+++ 1.x/localsim/models/tso/controller.py
@@ -9,6 +9,33 @@ from localsim.models.infra.control import concrete as control
 from localsim.models.infra import survey_zone
 
 
+def stub(ctm):
+    return pd.read_pickle('greentimes.pkl')
+
+def stoplight_timings(df, cell):
+    # Obtain a list of phase timings for involved phases
+    phase_timings = map(
+        lambda c: list(df[df.cell == c].sort_values(by='timestep').is_green.astype('int16').values),
+        const.REVERSE_PHASE_MAPPING[cell]
+    )
+
+    print(phase_timings)
+
+    # Reduce the phase timings into 1 list
+    signal_timings = phase_timings[0]
+    for pt in phase_timings:
+        for s in range(len(signal_timings)):
+            signal_timings[s] |= pt[s]
+
+    # Convert into seconds
+    output = []
+    for s in signal_timings:
+        output += [s] * const.TIME_STEP
+
+    # Process the output
+    #print("{}: {}".format(cell, output))
+    return output
+
 class TSO(object):
     '''Maintains a set of stoplights and survey zones, then passes information from them over to the linear solver later on.'''
 
@@ -17,14 +44,18 @@ class TSO(object):
         self.vol_observer = vol_observer
         self.controls = scene.controls
         self.survey_zones = scene.surveyors
+
         self.epoch = 0
         self.cell_map = {} # survey.id -> tuple
         self.ctm = collections.defaultdict(lambda: 0) # tuple -> value
+        self.greentimes = [] # [epoch] -> dataframe
 
         for road in self.controls:
             for control in self.controls[road]:
                 control.controlled = True
                 control.connect('recompute', self._signal_callback)
+                control.state_list = stoplight_timings(stub({}), const.STOPLIGHT_MAPPING[road.label])
+                print("{} greentimes: \n{}\n".format(road.label, control.state_list))
 
         for road in self.survey_zones:
             for survey in self.survey_zones[road]:
@@ -43,9 +74,11 @@ class TSO(object):
                 for k in network_state:
                     self.ctm[self.cell_map[k]] += network_state[k]
 
-                print(self.ctm)
+                # 2. Pass the state to the solver, then solve
                 print("Running solver...")
-                time.sleep(10)
+                _result = stub(self.ctm)
+                self.greentimes.append(_result)
                 self.epoch += 1
+
             print("Setting greentimes...")
             source.epoch += 1
